d1a9e19cd414a55f8d23b3e1115e2c9f8f28fe25
MartinCiro <52991306+MartinCiro@users.noreply.github.com>
2024-01-03 19:20:15 -0500

    se elimina funciones sin uso

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Tasklist from './components/TaskList.jsx';
 import TaskForm from './components/TaskForm.jsx';
 import { Container } from '@mui/material';
-import Navbar from './components/Navbar'
-import CreateItem from './components/CreateItem.jsx';
 import Home from './components/Home.jsx';
 
 export default function App() {
